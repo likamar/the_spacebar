919bd98b3b7351dca11da23c8ecf66ce7deaa9ff
Marcin Lika <lika.marcin@gmail.com>
2020-05-08 13:53:27 +0200

    Create show funcion in ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,15 +1,28 @@
 <?php
 
-
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('Hello world!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show one space article: %s',
+            $slug
+        ));
     }
 }
