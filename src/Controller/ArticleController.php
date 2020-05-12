0aee4f1ccb0a38bf0899dc693b617b681ee6bbd4
Marcin Lika <lika.marcin@gmail.com>
2020-05-12 15:23:12 +0200

    Fix homepage and article routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,11 +13,11 @@ class ArticleController extends AbstractController //AbstractController helps to
      */
     public function homepage()
     {
-        return new Response('Hello world!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
